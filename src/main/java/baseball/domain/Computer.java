f877c6f9c4f571568b5582a60bfc61656ea45137
minzzun99 <skg06123@naver.com>
2024-08-16 22:07:56 +0900

    Feat: 랜덤 숫자 생성 기능 추가 Computer 클래스에 RandomNumberGenerator 클래스에서 생성한 랜덤 숫자 저장하는 기능을 추가함.

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,5 +1,22 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class Computer {
+    private static final int START_RANGE = 1;       // 랜덤 숫자 시작 범위
+    private static final int END_RANGE = 9;         // 랜덤 숫자 끝 범위
+    private static final int RANDOM_NUMBER_SIZE = 3;        // 랜덤 숫자의 개수
+    private final List<Integer> randomNumbers;       //랜던 숫자 저장
+
     // 컴퓨터 객체 생성
+    public Computer(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumbers = randomNumberGenerator.generateRandomNumbers(START_RANGE, END_RANGE, RANDOM_NUMBER_SIZE);
+    }
+
+    /*// 랜덤 숫자 생성 테스트
+    public static void main(String[] args) {
+        RandomNumberGenerator rmg = new RandomNumberGenerator();
+        Computer computer = new Computer(rmg);
+        computer.randomNumbers.stream().forEach(System.out::println);
+    }*/
 }
--- src/main/java/baseball/domain/RandomNumberGenerator.java
+++ src/main/java/baseball/domain/RandomNumberGenerator.java
@@ -1,5 +1,17 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class RandomNumberGenerator {
-    // 랜덤 숫자 생성기
+    public List<Integer> generateRandomNumbers(int start, int end, int size) {
+        Set<Integer> numberSet = new HashSet<>();
+        while (numberSet.size() < size) {
+            int number = Randoms.pickNumberInRange(start, end);
+            numberSet.add(number);
+        }
+        return numberSet.stream().toList();
+    }
 }
