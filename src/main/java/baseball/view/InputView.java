a0c20702dd5089192f935d19accf8b0604122a67
minzzun99 <skg06123@naver.com>
2024-08-17 23:06:20 +0900

    Feat: 게임 진행 문구 출력 기능 추가 InputView 클래스에 게임 진행에 필요한 문구를 출력하는 메서드를 추가함

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,21 @@
 package baseball.view;
 
 public class InputView {
+    private static final String GAME_START_MSG = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_MSG = "숫자를 입력해주세요 : ";
+
+    private static final String RESTART_OR_EXIT_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     // 입력을 처리
+    public static void printStart() {
+        System.out.println(GAME_START_MSG);
+    }
+
+    public static void printInputNumber() {
+        System.out.println(INPUT_MSG);
+    }
+
+    public static void printRestartOrExit() {
+        System.out.println(RESTART_OR_EXIT_MSG);
+    }
 }
