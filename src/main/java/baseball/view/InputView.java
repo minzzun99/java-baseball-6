c5ef7a43bf04c46306d8f38f259a2d6492b31ce0
minzzun99 <skg06123@naver.com>
2024-08-17 23:09:57 +0900

    Comment: InputView 클래스 주석 추가 InputView 클래스에 각 메서드에 주석을 추가함

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,15 +6,17 @@ public class InputView {
 
     private static final String RESTART_OR_EXIT_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    // 입력을 처리
+    // 게임 시작 메시지를 출력하는 메서드
     public static void printStart() {
         System.out.println(GAME_START_MSG);
     }
 
+    // 숫자를 입력 받는 메시지 출력하는 메서드
     public static void printInputNumber() {
         System.out.println(INPUT_MSG);
     }
 
+    // 게임 종료 여부를 묻는 메시지를 출력하는 메서드
     public static void printRestartOrExit() {
         System.out.println(RESTART_OR_EXIT_MSG);
     }
