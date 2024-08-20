97d5c8f32e316c045d864254aeb91583207ed243
minzzun99 <skg06123@naver.com>
2024-08-20 17:32:00 +0900

    Feat: Player 클래스 구현 Player 클래스 구현함

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,5 +1,31 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     // 사용자에게 입력 받은 세개의 숫자를 저장하는 객체
+    private List<Integer> inputNumbers;
+
+    public Player(String numberStr) {
+        try {
+            Validation.validationInputNumber(numberStr);
+            this.inputNumbers = numberToList(numberStr);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            throw new IllegalArgumentException("사용자 입력 예외 발생");
+        }
+    }
+
+    private static List<Integer> numberToList(String s) {
+        List<Integer> numberList = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            numberList.add(Character.getNumericValue(c));
+        }
+        return numberList;
+    }
+
+    public List<Integer> getInputNumbers() {
+        return inputNumbers;
+    }
 }
