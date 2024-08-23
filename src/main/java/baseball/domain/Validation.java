5ba4d7f328d07e47a9cff7c20a7abc55e54e0263
minzzun99 <skg06123@naver.com>
2024-08-23 22:40:39 +0900

    Feat: Validation 예외 메시지 추가

--- src/main/java/baseball/domain/Validation.java
+++ src/main/java/baseball/domain/Validation.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class Validation {
     public static void validationInputNumber(String input) {
         if (input.length() != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("세자리의 숫자를 입력해주세요.");
         }
 
         Set<Integer> numberSet = new HashSet<>();
@@ -15,7 +15,7 @@ public class Validation {
 
             // 숫자가 1~9 범위에 있는지 확인
             if (number < 1 || number > 9) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("1 ~ 9 사이의 숫자를 입력해주세요.");
             }
 
             numberSet.add(number);
@@ -23,13 +23,13 @@ public class Validation {
 
         // 중복 여부 확인
         if (numberSet.size() != 3) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("중복되지 않은 숫자를 입력해주세요.");
         }
     }
 
     public static void validationRestartInput(String input) {
         if (!(input.equals("1") || input.equals("2"))) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1과 2의 숫자만 입력해주세요.");
         }
     }
 }
